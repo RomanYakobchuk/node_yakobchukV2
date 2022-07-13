0224e08a6b7c3188ee62818515ebe7ad6fa92b5a
Roman Yakobchuk <71780751+RomanYakobchuk@users.noreply.github.com>
2022-07-13 21:40:45 +0300

    Delete config.js

--- constants/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PORT: 5000,
-    MONGO_URL: `mongodb+srv://yakobchukrv:hjvfydfk28@cluster0.f87ap.mongodb.net/?retryWrites=true&w=majority`,
-
-    ACCESS_TOKEN_SECRET: 'asd',
-    REFRESH_TOKEN_SECRET: 'qwe',
-};
